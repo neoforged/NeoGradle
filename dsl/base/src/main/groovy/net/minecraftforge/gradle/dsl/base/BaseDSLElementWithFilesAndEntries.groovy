95ec61d0372e992a21bd36dbc8d33e3e5ef777d4
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-18 16:52:46 +0200

    Enable groovydoc and add missing dependency on transformer

--- dsl/base/src/main/groovy/net/minecraftforge/gradle/dsl/base/BaseDSLElementWithFilesAndEntries.groovy
+++ dsl/base/src/main/groovy/net/minecraftforge/gradle/dsl/base/BaseDSLElementWithFilesAndEntries.groovy
@@ -1,30 +1,34 @@
 package net.minecraftforge.gradle.dsl.base
 
-
-import org.gradle.api.file.ConfigurableFileCollection;
-import org.gradle.api.provider.ListProperty;
+import groovy.transform.CompileStatic
+import net.minecraftforge.gradle.dsl.annotations.DSLProperty
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.provider.ListProperty
 
 /**
  * Represents part of an extension which combines a set of files with entries as well as raw addable entries.
  *
  * @param <TSelf> The type of the implementing class.
  */
+@CompileStatic
 interface BaseDSLElementWithFilesAndEntries<TSelf extends BaseDSLElementWithFilesAndEntries<TSelf>> extends ProjectAssociatedBaseDSLElement<TSelf> {
 
     /**
-     * @returns The files which contain entries relevant to this extension.
+     * @return The files which contain entries relevant to this extension.
      */
-    ConfigurableFileCollection getFiles();
+    @DSLProperty
+    ConfigurableFileCollection getFiles()
 
     /**
-     * @returns The raw additional entries relevant to this extension.
+     * @return The raw additional entries relevant to this extension.
      */
-    ListProperty<String> getEntries();
+    @DSLProperty(propertyName = 'entry')
+    ListProperty<String> getEntries()
 
     /**
      * Indicates if either at least one file is specified or at least one additional raw entry is specified.
      *
-     * @return {@code true}, when at least one file or entry is specified. False otherwise.
+     * @return {@code true}, when at least one file or entry is specified, {@code false} otherwise.
      */
-    boolean isEmpty();
+    boolean isEmpty()
 }
