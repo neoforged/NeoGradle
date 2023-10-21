2b06558c3c3b57a088d05f477ad47491ac30d557
Marc Hermans <marc.hermans@ldtteam.com>
2023-10-22 00:09:20 +0200

    Fix the missing annotations.

--- dsl/common/src/main/groovy/net/neoforged/gradle/dsl/common/runtime/tasks/NamedFileRef.groovy
+++ dsl/common/src/main/groovy/net/neoforged/gradle/dsl/common/runtime/tasks/NamedFileRef.groovy
@@ -1,8 +1,16 @@
 package net.neoforged.gradle.dsl.common.runtime.tasks
 
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+
 interface NamedFileRef {
 
+    @Input
     String getName()
 
+    @InputFile
+    @PathSensitive(PathSensitivity.NONE)
     File getFile()
 }
