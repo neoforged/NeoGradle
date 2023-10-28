c6bb9acd40aeff51bd4a93a82683c185916de76c
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-10-28 11:46:49 +0200

    Build only NG_ branches going forward

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -38,7 +38,6 @@ project {
         text("env.PUBLISHED_JAVA_ARTIFACT_ID", "NeoGradle", label = "Published artifact id", description = "The maven coordinate artifact id that has been published by this build. Can not be empty.", allowEmpty = false)
         text("env.PUBLISHED_JAVA_GROUP", "net.neoforged.gradle", label = "Published group", description = "The maven coordinate group that has been published by this build. Can not be empty.", allowEmpty = false)
         text("git_branch_spec", """
-                +:refs/heads/(FG_*)
                 +:refs/heads/(NG_*)
             """.trimIndent(), label = "The branch specification of the repository", description = "By default all main branches are build by the configuration. Modify this value to adapt the branches build.", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("docker_jdk_version", "8", label = "JDK version", description = "The version of the JDK to use during execution of tasks in a JDK.", display = ParameterDisplay.HIDDEN, allowEmpty = false)
