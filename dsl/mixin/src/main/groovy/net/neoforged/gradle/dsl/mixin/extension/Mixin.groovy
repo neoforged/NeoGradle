2ed791434f0555fea162f9f3b25612e377517f6a
Marc Hermans <marc.hermans@ldtteam.com>
2023-11-01 16:28:37 +0100

    Add support for per Run and per Jar task configs. Closes: #14

--- dsl/mixin/src/main/groovy/net/neoforged/gradle/dsl/mixin/extension/Mixin.groovy
+++ dsl/mixin/src/main/groovy/net/neoforged/gradle/dsl/mixin/extension/Mixin.groovy
@@ -4,11 +4,15 @@ import groovy.transform.CompileStatic
 import net.minecraftforge.gdi.BaseDSLElement
 import net.minecraftforge.gdi.annotations.DSLProperty
 import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 
 @CompileStatic
 interface Mixin extends BaseDSLElement<Mixin> {
     String EXTENSION_NAME = "mixin";
 
     @DSLProperty
+    @Optional
+    @Input
     SetProperty<String> getConfigs();
 }
