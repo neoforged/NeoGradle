00c4533625b80684e532ed265544656cd0fe8547
Marc Hermans <marc.hermans@ldtteam.com>
2023-11-02 20:30:08 +0100

    Fix gradle being stupid when generating set properties.
    Closes: #29

--- mixin/src/main/java/net/neoforged/gradle/mixin/MixinExtension.java
+++ mixin/src/main/java/net/neoforged/gradle/mixin/MixinExtension.java
@@ -3,20 +3,28 @@ package net.neoforged.gradle.mixin;
 import net.minecraftforge.gdi.ConfigurableDSLElement;
 import net.neoforged.gradle.dsl.mixin.extension.Mixin;
 import org.gradle.api.Project;
+import org.gradle.api.provider.SetProperty;
 
 import javax.inject.Inject;
 
 public abstract class MixinExtension implements Mixin, ConfigurableDSLElement<Mixin> {
 
     private final Project project;
+    private final SetProperty<String> configs;
 
     @Inject
     public MixinExtension(Project project) {
         this.project = project;
+        this.configs = project.getObjects().setProperty(String.class);
     }
 
     @Override
     public Project getProject() {
         return project;
+    }
+    
+    @Override
+    public SetProperty<String> getConfigs() {
+        return configs;
     }
 }
