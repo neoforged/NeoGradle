414b65d4417597f06ce72ec1ec43e33d4e22eb3d
Marc Hermans <marc.hermans@ldtteam.com>
2024-02-19 18:14:27 +0100

    Prepare for configuration cache

--- neoform/src/main/java/net/neoforged/gradle/neoform/runtime/tasks/InjectFromDirectorySource.java
+++ neoform/src/main/java/net/neoforged/gradle/neoform/runtime/tasks/InjectFromDirectorySource.java
@@ -1,13 +1,20 @@
 package net.neoforged.gradle.neoform.runtime.tasks;
 
 import net.neoforged.gradle.util.ZipBuildingFileTreeVisitor;
+import org.gradle.api.file.ArchiveOperations;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.ConfigurableFileTree;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.file.FileTree;
+import org.gradle.api.file.ProjectLayout;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.PathSensitive;
 import org.gradle.api.tasks.PathSensitivity;
 import org.jetbrains.annotations.Nullable;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -22,6 +29,15 @@ public abstract class InjectFromDirectorySource extends AbstractInjectSource {
     @PathSensitive(PathSensitivity.NONE)
     public abstract DirectoryProperty getDirectory();
 
+    /**
+     * The object factory that can be used to manage the internal subsystems of a gradle model.
+     * Allows for the creation of for example file collections, trees and other components.
+     *
+     * @return The object factory.
+     */
+    @Inject
+    public abstract ObjectFactory getObjectFactory();
+
     @Override
     public byte @Nullable [] tryReadFile(String path) throws IOException {
         File packageInfoTemplateFile = getDirectory().file("package-info-template.java").get().getAsFile();
@@ -33,7 +49,8 @@ public abstract class InjectFromDirectorySource extends AbstractInjectSource {
 
     @Override
     public void copyTo(ZipOutputStream out) throws IOException {
-        FileTree source = getProject().fileTree(getDirectory()).matching(createFilter());
+        final ConfigurableFileTree outputTree = getObjectFactory().fileTree();
+        FileTree source = outputTree.from(getDirectory()).matching(createFilter());
         source.visit(new ZipBuildingFileTreeVisitor(out));
     }
 }
