f05a9f1c4b3092376e67356fb7672e3ef804c808
Marc Hermans <marc.hermans@ldtteam.com>
2024-02-19 19:37:53 +0100

    Properly remove language prefix Create proper task name Create proper resource name

--- .github/scripts/collect-tests.sh
+++ .github/scripts/collect-tests.sh
@@ -2,4 +2,10 @@
 
 ./gradlew determineTests | grep -e '<< TEST >>' | sed -e 's/<< TEST >>//g' > ./tasks
 TESTS=$(cat tasks | jq --raw-input . | jq --compact-output --slurp .)
+# Check if the GITHUB_OUTPUT is set
+if [ -z "$GITHUB_OUTPUT" ]; then
+  # We do not have github output, then use the set output command
+  echo "::set-output name=tests-to-run::$TESTS"
+  exit 0
+fi
 echo "tests-to-run=$TESTS" >> "$GITHUB_OUTPUT"
