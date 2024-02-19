6ec2408f4bd56581303a1b578f713d695a7ca7d3
Marc Hermans <marc.hermans@ldtteam.com>
2024-02-19 19:08:29 +0100

    Output to the github output variable instead of the command Use a flag to only register the tasks if needed.

--- .github/scripts/collect-tests.sh
+++ .github/scripts/collect-tests.sh
@@ -2,4 +2,4 @@
 
 ./gradlew determineTests | grep -e '<< TEST >>' | sed -e 's/<< TEST >>//g' > ./tasks
 TESTS=$(cat tasks | jq --raw-input . | jq --compact-output --slurp .)
-echo "::set-output name=tests-to-run::$TESTS"
+echo "tests-to-run=$TESTS" >> "$GITHUB_OUTPUT"
