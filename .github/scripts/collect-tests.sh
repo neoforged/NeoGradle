917f9a845e02371a6c117d64ebbb9daaac6e2177
Marc Hermans <marc.hermans@ldtteam.com>
2024-02-19 19:03:47 +0100

    Fix the test collection not stripping the test prefix marker

--- .github/scripts/collect-tests.sh
+++ .github/scripts/collect-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./gradlew determineTests | grep -e '<< TEST >>' | sed -e 's/<< TEST >> //g' > ./tasks
+./gradlew determineTests | grep -e '<< TEST >>' | sed -e 's/<< TEST >>//g' > ./tasks
 TESTS=$(cat tasks | jq --raw-input . | jq --compact-output --slurp .)
 echo "::set-output name=tests-to-run::$TESTS"
