58f53fa288265eed04213f70b3aeeea61618c4fe
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-17 18:01:41 -0500

    Expose configurations via DSL

--- common/src/main/java/net/neoforged/gradle/common/extensions/AccessTransformersExtension.java
+++ common/src/main/java/net/neoforged/gradle/common/extensions/AccessTransformersExtension.java
@@ -1,15 +1,45 @@
 package net.neoforged.gradle.common.extensions;
 
+import net.neoforged.gradle.common.CommonProjectPlugin;
 import net.neoforged.gradle.common.extensions.base.BaseFilesWithEntriesExtension;
 import net.neoforged.gradle.dsl.common.extensions.AccessTransformers;
+import org.gradle.api.Action;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.ConfigurablePublishArtifact;
+import org.gradle.api.artifacts.dsl.ArtifactHandler;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
 
 import javax.inject.Inject;
 
 public abstract class AccessTransformersExtension extends BaseFilesWithEntriesExtension<AccessTransformers> implements AccessTransformers {
+    private transient final DependencyHandler projectDependencies;
+    private transient final ArtifactHandler projectArtifacts;
 
     @Inject
     public AccessTransformersExtension(Project project) {
         super(project);
+
+        this.projectDependencies = project.getDependencies();
+        this.projectArtifacts = project.getArtifacts();
+    }
+
+    @Override
+    public void consume(Object notation) {
+        this.projectDependencies.add(CommonProjectPlugin.ACCESS_TRANSFORMER_CONFIGURATION, notation);
+    }
+
+    @Override
+    public void consumeApi(Object notation) {
+        this.projectDependencies.add(CommonProjectPlugin.ACCESS_TRANSFORMER_API_CONFIGURATION, notation);
+    }
+
+    @Override
+    public void expose(Object path, Action<ConfigurablePublishArtifact> action) {
+        projectArtifacts.add(CommonProjectPlugin.ACCESS_TRANSFORMER_ELEMENTS_CONFIGURATION, path, action);
+    }
+
+    @Override
+    public void expose(Object path) {
+        expose(path, artifacts -> {});
     }
 }
--- dsl/common/src/main/groovy/net/neoforged/gradle/dsl/common/extensions/AccessTransformers.groovy
+++ dsl/common/src/main/groovy/net/neoforged/gradle/dsl/common/extensions/AccessTransformers.groovy
@@ -2,10 +2,20 @@ package net.neoforged.gradle.dsl.common.extensions
 
 import groovy.transform.CompileStatic
 import net.minecraftforge.gdi.BaseDSLElementWithFilesAndEntries
+import org.gradle.api.Action
+import org.gradle.api.artifacts.ConfigurablePublishArtifact
+import org.gradle.api.artifacts.dsl.DependencyCollector
 
 /**
  * Defines a DSL extension which allows for the specification of access transformers.
  */
 @CompileStatic
 interface AccessTransformers extends BaseDSLElementWithFilesAndEntries<AccessTransformers> {
+    void consume(Object notation)
+
+    void consumeApi(Object notation)
+
+    void expose(Object path)
+
+    void expose(Object path, Action<ConfigurablePublishArtifact> action)
 }
