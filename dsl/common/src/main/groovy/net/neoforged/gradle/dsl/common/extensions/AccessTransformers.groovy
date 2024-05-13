8faf299fd8aac266e316f5d1c23580e850d4daf6
Luke Bemish <lukebemish@lukebemish.dev>
2024-05-13 07:13:52 -0500

    Add system for publishing and consuming ATs (#146)
    Co-authored-by: Marc Hermans <marc.hermans@ldtteam.com>

--- dsl/common/src/main/groovy/net/neoforged/gradle/dsl/common/extensions/AccessTransformers.groovy
+++ dsl/common/src/main/groovy/net/neoforged/gradle/dsl/common/extensions/AccessTransformers.groovy
@@ -2,10 +2,23 @@ package net.neoforged.gradle.dsl.common.extensions
 
 import groovy.transform.CompileStatic
 import net.minecraftforge.gdi.BaseDSLElementWithFilesAndEntries
+import org.gradle.api.Action
+import org.gradle.api.artifacts.ConfigurablePublishArtifact
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.dsl.DependencyCollector
 
 /**
  * Defines a DSL extension which allows for the specification of access transformers.
  */
 @CompileStatic
 interface AccessTransformers extends BaseDSLElementWithFilesAndEntries<AccessTransformers> {
+    Dependency consume(Object notation)
+
+    Dependency consumeApi(Object notation)
+
+    void expose(Dependency dependency)
+
+    void expose(Object path)
+
+    void expose(Object path, Action<ConfigurablePublishArtifact> action)
 }
