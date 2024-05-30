3719ffc55555c264c5569bb24d880c21af40c7d9
Marc Hermans <marc.hermans@ldtteam.com>
2024-05-30 18:47:58 +0200

    No pretty print

--- .github/scripts/collect-tests.sh
+++ .github/scripts/collect-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TESTS=$(./gradlew determineTests | grep -e '<< TEST >>' | sed -e 's/<< TEST >>//g' | jq -s 'add')
+TESTS=$(./gradlew determineTests | grep -e '<< TEST >>' | sed -e 's/<< TEST >>//g' | jq -s 'add' | jq -c .)
 # Check if the GITHUB_OUTPUT is set
 if [ -z "$GITHUB_OUTPUT" ]; then
   # We do not have github output, then use the set output command
