1225567381a3107d7e8fe2ce36a21064689b839f
Marc Hermans <marc.hermans@ldtteam.com>
2024-07-28 17:33:02 +0200

    Make the run test scope be set to a given pattern by default.

--- common/src/main/java/net/neoforged/gradle/common/runs/run/RunTestScopeImpl.java
+++ common/src/main/java/net/neoforged/gradle/common/runs/run/RunTestScopeImpl.java
@@ -2,16 +2,36 @@ package net.neoforged.gradle.common.runs.run;
 
 import net.neoforged.gradle.dsl.common.runs.run.RunTestScope;
 import org.gradle.api.Project;
+import org.gradle.api.file.Directory;
 
 import javax.inject.Inject;
+import java.io.File;
+
 
 public abstract class RunTestScopeImpl implements RunTestScope {
+
+    private static final String DEFAULT_PATTERN = ".*";
+    private static final String NOTHING_DEFINED_MARKER = "__________NOTHING_DEFINED__________";
 
     private final Project project;
 
     @Inject
     public RunTestScopeImpl(Project project) {
         this.project = project;
+
+        getPattern().convention(
+                project.provider(() -> {
+                    if (getPackageName().orElse(getDirectory().map(Directory::getAsFile).map(File::getName))
+                            .orElse(getClassName())
+                            .orElse(getMethod())
+                            .orElse(getCategory())
+                            .getOrNull() == null) {
+                        return NOTHING_DEFINED_MARKER;
+                    }
+
+                    return null;
+                })
+        );
     }
 
     @Override
