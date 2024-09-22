d74439269e6c73a56eb2ea428b008c12c40fa023
Marc Hermans <marc.hermans@ldtteam.com>
2024-09-22 11:28:30 +0200

    Fix unit test

--- userdev/src/functionalTest/groovy/net/neoforged/gradle/userdev/RunTests.groovy
+++ userdev/src/functionalTest/groovy/net/neoforged/gradle/userdev/RunTests.groovy
@@ -613,8 +613,10 @@ class RunTests extends BuilderBasedTestSpecification {
         def secondSection = lines[indexes[1]..indexes[2] - 2]
         def thirdSection = lines[indexes[2]..indexes[3] - 2]
 
-        firstSection.find { it.contains("Working Directory:") }.toString().split(":")[1].trim().endsWith("runs_have_configurable_working_directories_with_default/runs/aClient".replace("/", File.separator))
-        secondSection.find { it.contains("Working Directory:") }.toString().split(":")[1].trim().endsWith("runs_have_configurable_working_directories_with_default/runs/bClient".replace("/", File.separator))
-        thirdSection.find { it.contains("Working Directory:") }.toString().split(":")[1].trim().endsWith("runs_have_configurable_working_directories_with_default/clientThree".replace("/", File.separator))
+        def prefix = "Working Directory:"
+
+        firstSection.find { it.contains(prefix) }.toString().replace(prefix, "").trim().endsWith("runs_have_configurable_working_directories_with_default/runs/aClient".replace("/", File.separator))
+        secondSection.find { it.contains(prefix) }.toString().replace(prefix, "").trim().endsWith("runs_have_configurable_working_directories_with_default/runs/bClient".replace("/", File.separator))
+        thirdSection.find { it.contains(prefix) }.toString().replace(prefix, "").trim().endsWith("runs_have_configurable_working_directories_with_default/clientThree".replace("/", File.separator))
     }
 }
